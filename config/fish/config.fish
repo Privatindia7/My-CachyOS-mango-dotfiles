c056e48510d03c09f3048d661ba0ed0ce323b336
Privatindia7 <v.kuttinal@googlemail.com>
2026-03-17 23:48:02 +0100

    Fix config.fish: remove duplicate env vars

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,8 +17,4 @@ if status is-login
     exec mango
 end
 
-set -x __GL_GSYNC_ALLOWED 0
-set -x __GL_VRR_ALLOWED 0
-set -x GBM_BACKEND nvidia-drm
-set -x __GLX_VENDOR_LIBRARY_NAME nvidia
-set -x WLR_NO_HARDWARE_CURSORS 1
+
