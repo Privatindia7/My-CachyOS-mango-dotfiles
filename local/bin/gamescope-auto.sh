c593b22f1c4ffb59aba8c0d2e2d7598d7aa749e5
Privatindia7 <v.kuttinal@googlemail.com>
2026-04-20 00:00:01 +0200

    Auto-update 2026-04-20

--- local/bin/gamescope-auto.sh
+++ local/bin/gamescope-auto.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-niri msg action focus-workspace "gaming"
-sleep 0.5
-if wlr-randr | grep -q "DP-"; then
+export XKB_DEFAULT_LAYOUT=de
+export XKB_DEFAULT_VARIANT=
+if niri msg outputs | grep -q "DP-"; then
     exec gamescope -f -r 144 -W 2560 -H 1440 -w 1280 -h 960 --force-grab-cursor --scaler stretch -- "$@"
 else
     exec gamescope -f -r 75 -W 1440 -H 900 -w 1280 -h 960 --force-grab-cursor --scaler stretch -- "$@"
